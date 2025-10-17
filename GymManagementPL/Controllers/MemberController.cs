dead22ae261e88903b64199a473d94559c1e40d1
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-10-18 02:52:10 +0300

    MVCSession05

---
+++ GymManagementPL/Controllers/MemberController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementPL.Controllers
+{ 
+    public class MemberController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+
+        public IActionResult GetMembers()
+        {
+             return View();
+        }
+
+        public IActionResult CreateMember()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ GymManagementPL/Controllers/TrainerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementPL.Controllers
+{
+    public class TrainerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetTrainer()
+        {
+            return View();
+        }
+
+        public IActionResult CreateTrainer()
+        {
+            return View();
+        }
+
+    }
+}
