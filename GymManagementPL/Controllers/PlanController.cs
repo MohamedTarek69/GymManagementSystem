037268fff4132868122a4e2af2080e33c98c9906
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-10-25 02:26:23 +0300

    MVCSession07

---
+++ GymManagementPL/Controllers/PlanController.cs
@@ -0,0 +1,133 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels.PlanViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementPL.Controllers
+{
+    public class PlanController : Controller
+    {
+        private readonly IPlanService _planService;
+
+        public PlanController(IPlanService planService)
+        {
+            _planService = planService;
+        }
+
+        public ActionResult Index()
+        {
+            var plans = _planService.GetAllPlans();
+            return View(plans);
+        }
+
+        public ActionResult Details(int id)
+        {
+            if(id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid plan ID.";
+                return RedirectToAction("Index");
+            }
+            var plan = _planService.GetPlanDetails(id);
+            if (plan == null)
+            {
+                TempData["ErrorMessage"] = "Plan not found.";
+                return RedirectToAction("Index");
+            }
+            return View(plan);
+        }
+
+        public ActionResult Edit(int id)
+        {
+           if(id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid plan ID.";
+                return RedirectToAction("Index");
+            }
+            var plan = _planService.GetPlanToUpdate(id);
+            if (plan == null)
+            {
+                TempData["ErrorMessage"] = "Failed to edit plan.";
+                return RedirectToAction("Index");
+            }
+            return View(plan);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([FromRoute]int id, UpdatePlanViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("WrongData", "Please Check The Validation Of The Data");
+                return View(model);
+            }
+
+            var result = _planService.UpdatePlanDetails(id, model);
+            if (!result)
+            {
+                TempData["ErrorMessage"] = "Failed to update plan.";
+                return View(model);
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "Plan updated successfully.";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Activate(int id)
+        {
+            if (id <= 0) {
+                TempData["ErrorMessage"] = "Invalid plan ID.";
+                return RedirectToAction("Index");
+            }
+            var result = _planService.ToggleStatus(id);
+            if (!result)
+            {
+                TempData["ErrorMessage"] = "Failed to update plan status.";
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "Plan status updated successfully.";
+            }
+            return RedirectToAction("Index");
+        }
+
+        #region If both action do the same the same 
+        //public ActionResult Details(int id, string ViewName = "Details")
+        //   {
+        //       if (id <= 0)
+        //       {
+        //           TempData["ErrorMessage"] = "Invalid plan ID.";
+        //           return RedirectToAction("Index");
+        //       }
+        //       var plan = _planService.GetPlanDetails(id);
+        //       if (plan == null)
+        //       {
+        //           TempData["ErrorMessage"] = "Plan not found.";
+        //           return RedirectToAction("Index");
+        //       }
+        //       return View(ViewName, plan);
+        //   }
+
+        //public ActionResult Edit(int id)
+        //   {
+        //       //if(id <= 0)
+        //       // {
+        //       //     TempData["ErrorMessage"] = "Invalid plan ID.";
+        //       //     return RedirectToAction("Index");
+        //       // }
+        //       // var plan = _planService.GetPlanToUpdate(id);
+        //       // if (plan == null)
+        //       // {
+        //       //     TempData["ErrorMessage"] = "Plan not found.";
+        //       //     return RedirectToAction("Index");
+        //       // }
+        //       // return View(plan);
+        //       return Details(id, "Edit");
+        //   }
+
+        #endregion
+    }
+
+
+}
---
+++ GymManagementPL/Controllers/SessionController.cs
@@ -0,0 +1,163 @@
+﻿using GymManagementBLL.Services.Classes;
+using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels.SessionViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GymManagementPL.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly ISessionService _sessionService;
+
+        public SessionController(ISessionService sessionService)
+        {
+            _sessionService = sessionService;
+        }
+
+        public ActionResult Index()
+        {
+            var sessions = _sessionService.GetAllSessions();
+            return View(sessions);
+        }
+
+        public ActionResult Details(int id)
+        {
+            if(id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid session ID.";
+                return RedirectToAction("Index");
+
+            }
+            var session = _sessionService.GetSessionById(id);
+            if(session == null)
+            {
+                TempData["ErrorMessage"] = "Session not found.";
+                return RedirectToAction("Index");
+            }
+            return View(session);
+
+        }
+
+        public ActionResult Create()
+        {
+
+            LoadTrainersDropDowns();
+            LoadCategoriesDropDowns();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateSessionViewModel createSession)
+        {
+            if (!ModelState.IsValid)
+            {
+                LoadTrainersDropDowns();
+                LoadCategoriesDropDowns();
+                return View(createSession);
+            }
+            var result = _sessionService.CreateSession(createSession);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Session created successfully.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Failed to create session.";
+                LoadTrainersDropDowns();
+                LoadCategoriesDropDowns();
+                return View(createSession);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid session ID.";
+                return RedirectToAction("Index");
+            }
+            var session = _sessionService.GetSessionToUpdate(id);
+            if (session == null) {
+                TempData["ErrorMessage"] = "Session not found.";
+                return RedirectToAction("Index");
+            }
+            LoadTrainersDropDowns();
+            return View(session);
+        }
+        [HttpPost]
+        public ActionResult Edit([FromRoute]int id, UpdateSessionViewModel updateSession)
+        {
+            if (!ModelState.IsValid)
+            {
+                LoadTrainersDropDowns();
+                return View(updateSession);
+            }
+            var result = _sessionService.UpdateSession(id,updateSession);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Session updated successfully.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Failed to update session.";
+                LoadTrainersDropDowns();
+                return View(updateSession);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "ID Of Session Cannot Be 0 Or Negative Number";
+                return RedirectToAction(nameof(Index));
+            }
+            var session = _sessionService.GetSessionById(id);
+            if (session is null)
+            {
+                TempData["ErrorMessage"] = "Session Not Found";
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.SessionId = id;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var result = _sessionService.RemoveSession(id);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Session Deleted Successfully";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Failed To Delete Session";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        #region Helper Methods
+
+        public void LoadTrainersDropDowns()
+        {
+            var trainers = _sessionService.GetAllTrainersForDropDown();
+            ViewBag.Trainers = new SelectList(trainers, "Id", "Name");
+        }
+
+        public void LoadCategoriesDropDowns()
+        {
+            var categories = _sessionService.GetAllCategoriesForDropDown();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+        }
+
+        #endregion
+
+    }
+}
